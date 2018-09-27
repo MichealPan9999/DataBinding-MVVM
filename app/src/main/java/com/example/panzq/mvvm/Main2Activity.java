2e37aacf735c8ed57c6ed4c72feed3e8f1b5f6c8
MichealPan9999 <panziqiang99@gmail.com>
2018-09-27 20:43:48 +0800

    DataBinding 配置xml中data其他用法.

---
+++ app/src/main/java/com/example/panzq/mvvm/Main2Activity.java
@@ -0,0 +1,37 @@
+package com.example.panzq.mvvm;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.panzq.mvvm.bean.UserBean;
+import com.example.panzq.mvvm.databinding.ActivityMain2Binding;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main2);
+        ActivityMain2Binding binding = DataBindingUtil.setContentView(this, R.layout.activity_main2);
+        List<String> list = new ArrayList<>();
+        list.add("List1");
+        list.add("List2");
+        binding.setList(list);
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("key0", "map_value0");
+        map.put("key1", "map_value1");
+        binding.setMap(map);
+
+        String[] array = {"字符串1", "字符串2"};
+        binding.setArray(array);
+
+        UserBean userBean = new UserBean("张三",18);
+        binding.setMuser(userBean);
+    }
+}
